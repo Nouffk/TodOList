edb5025b5b48a62c627b3862a83b7b4c1bc354eb
Nouf Faisal <nouf@alMacBook-Air-alkhas-bNouf.local>
2024-04-17 18:38:52 +0300

    initial project

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nouf Faisal  on 08/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
